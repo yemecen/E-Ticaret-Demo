e415ed4b008decf26db0b14a74013e9198d9d80f
yemecen <y.emecen@gmail.com>
2018-02-04 21:18:53 +0300

    Product Model ve Migrate
    Product Model ve Migrate işlemleri

---
+++ database/migrations/2018_02_04_175349_products_table_create.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ProductsTableCreate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('name',200);
+            $table->text('description');
+            $table->integer('brandId');
+            $table->string('brandName',50);
+            $table->integer('categoryId');
+            $table->string('categoryName',50);
+            $table->string('sku',100);
+            $table->integer('stock');
+            $table->integer('tax',50);
+            $table->string('category',50);
+            $table->string('image',200);
+            $table->decimal('price',8,2);
+            $table->string('priceNot',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
